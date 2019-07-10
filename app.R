4c667feafbd844d1433d77e71379f140878aaf50
jemitchel <jonathan.mitchel3@gmail.com>
2019-07-10 11:49:16 -0400

    added horizontal scroll

--- app.R
+++ app.R
@@ -21,9 +21,9 @@ ui <- dashboardPage(
     actionButton(inputId = "exampleCrossProbes",
                  label = "Load probes crosshybridized with athymic nude mice - Hollingshead et al. study",
                  style="white-space: normal;
-                        height:45px;
-                        width:250px;
-                        font-size: 12px;"),
+                 height:45px;
+                 width:250px;
+                 font-size: 12px;"),
     verbatimTextOutput("hybridFileName"),
     textOutput("addLine2"),
     verbatimTextOutput("errorBox"),
@@ -36,34 +36,140 @@ ui <- dashboardPage(
     uiOutput("download2"),
     uiOutput("download3"),
     uiOutput("download4")
-  ),
+    ),
   dashboardBody(
     fluidRow(
       box(title = "NRSig", width = 12, textOutput("intro")),
-      column(width = 6, 
+      column(6,
              textOutput("tableTitle"),
-             textOutput("placeHolder"), 
+             textOutput("placeHolder"),
              tags$head(tags$style("#tableTitle{color: blue;
-                                 font-size: 20px;
+                                  font-size: 20px;
                                   }")),
              DT::dataTableOutput("resTable")),
-      column(width = 6, plotOutput("bplots"))
-    ),
-    div(style = "height:5000px;")
-  )
+      column(6,div(style = 'overflow-x: auto; height:850px;', plotOutput("bplots"))),
+    div(style = "height:1200px;"))
+    )
 )
+
+
+# ui <- fluidPage(
+#   sidebarLayout(
+#     sidebarPanel(width = 4,
+#       fileInput(inputId = "files",
+#                 label = "Upload .CEL files for one sample or a group of replicates (replicates will be averaged)",
+#                 multiple = TRUE),
+#       actionButton(inputId = "example",
+#                    label = "Use Example Input"),
+#       verbatimTextOutput("fileName"),
+#       textOutput("addLine1"),
+#       fileInput(inputId = "crossProbes",
+#                 label = "For xenograft samples only: Upload .csv of cross-hybridized probes to be removed (single column)",
+#                 multiple = FALSE),
+#       actionButton(inputId = "exampleCrossProbes",
+#                    label = "Load probes crosshybridized with athymic nude mice - Hollingshead et al. study",
+#                    style="white-space: normal;
+#                    height:45px;
+#                    width:250px;
+#                    font-size: 12px;"),
+#       verbatimTextOutput("hybridFileName"),
+#       textOutput("addLine2"),
+#       verbatimTextOutput("errorBox"),
+#       actionButton(inputId = "clear",
+#                    label = "Clear All Input"),
+#       actionButton(inputId = "compute",
+#                    label = "Compute",
+#                    style="color: #fff; background-color: #008000; border-color: #008000"),
+#       uiOutput("download1"),
+#       uiOutput("download2"),
+#       uiOutput("download3"),
+#       uiOutput("download4")
+#     ),
+# 
+#     mainPanel(
+#       titlePanel("NRSig"),
+#       textOutput("intro"),
+#       # box(title = "NRSig", width = 12, textOutput("intro")),
+#       column(width = 6,
+#              textOutput("tableTitle"),
+#              textOutput("placeHolder"),
+#              tags$head(tags$style("#tableTitle{color: blue;
+#                                   font-size: 20px;
+#                                   }")),
+#              DT::dataTableOutput("resTable",width = "100%")),
+#       column(width = 4, plotOutput("bplots")),
+#       div(style = "height:5000px;")
+#     )
+#   )
+# )
+
+
+# ui <- fixedPage(
+#   fixedRow(
+#     column(width=4,
+#       fileInput(inputId = "files",
+#                 label = "Upload .CEL files for one sample or a group of replicates (replicates will be averaged)",
+#                 multiple = TRUE),
+#       actionButton(inputId = "example",
+#                    label = "Use Example Input"),
+#       verbatimTextOutput("fileName"),
+#       textOutput("addLine1"),
+#       fileInput(inputId = "crossProbes",
+#                 label = "For xenograft samples only: Upload .csv of cross-hybridized probes to be removed (single column)",
+#                 multiple = FALSE),
+#       actionButton(inputId = "exampleCrossProbes",
+#                    label = "Load probes crosshybridized with athymic nude mice - Hollingshead et al. study",
+#                    style="white-space: normal;
+#                    height:45px;
+#                    width:250px;
+#                    font-size: 12px;"),
+#       verbatimTextOutput("hybridFileName"),
+#       textOutput("addLine2"),
+#       verbatimTextOutput("errorBox"),
+#       actionButton(inputId = "clear",
+#                    label = "Clear All Input"),
+#       actionButton(inputId = "compute",
+#                    label = "Compute",
+#                    style="color: #fff; background-color: #008000; border-color: #008000"),
+#       uiOutput("download1"),
+#       uiOutput("download2"),
+#       uiOutput("download3"),
+#       uiOutput("download4")),
+#     column(width=8, title = "NRSig", 
+#            textOutput("intro"),
+#            fixedRow(
+#              column(5,DT::dataTableOutput("resTable",width="70%")),
+#              column(3,plotOutput("bplots"))
+#            )
+#     )
+#   )
+# )
+#   # column(width = 9,
+#   #   fixedRow(box(title = "NRSig", width = 12, textOutput("intro")),
+#   #     column(width = 10,box(title = "NRSig", width = 12, textOutput("intro"))),
+#   #   fixedRow(
+#   #     column(width = 5,
+#   #            textOutput("tableTitle"),
+#   #            textOutput("placeHolder"),
+#   #            tags$head(tags$style("#tableTitle{color: blue;
+#   #                                 font-size: 20px;
+#   #                                 }")),
+#   #            DT::dataTableOutput("resTable")),
+#   #     column(width = 6, plotOutput("bplots"))
+#   #   )
+#   # )))
 
 
 server <- function(input, output, session) {
   # increases allowable size of file uploads
   options(shiny.maxRequestSize=35*1024^2)
-
+  
   rv <- reactiveValues() # container for selected files
   rv2 <- reactiveValues() # container for crosshybrid probes file
   nclicks <- reactiveVal(0) # reactive number times compute is clicked
   status <- reactiveValues("finished"="Results will be displayed here","title"="") 
   
-
+  
   output$intro <- renderText({
     "This program computes differentially expressed genes between a query MCF7 microarray sample
     and prior distributions generated from serum-starved MCF7 samples. This allows for
@@ -83,7 +189,7 @@ server <- function(input, output, session) {
     rv2$data <- input$crossProbes$datapath
     rv2$name <- input$crossProbes$name
   })
-
+  
   observeEvent(input$example, {
     rv$data <- c("./data/example.CEL")
     rv$name <- c("example.CEL")
@@ -102,7 +208,7 @@ server <- function(input, output, session) {
     rv2$data <- NULL
     rv2$name <- NULL
   })
-
+  
   output$fileName <- renderText({
     # tmp <- "No Files Uploaded"
     tmp <- NULL
@@ -183,10 +289,10 @@ server <- function(input, output, session) {
     return(NULL)
   }
   
-
+  
   res <- reactiveVal() # holds all calculation results
   chosenPlt <- reactiveVal() # holds output z-score figure
-
+  
   observeEvent(input$compute, {
     err1 <- celFileError(rv$data)
     err2 <- csvFileError(rv2$data)
@@ -198,7 +304,7 @@ server <- function(input, output, session) {
       rv$errorMessage <- err2
       return(NULL)
     }
-
+    
     if(nclicks() != 0){
       return(NULL)
     }
@@ -232,7 +338,7 @@ server <- function(input, output, session) {
     status$title <- "Enriched NR-Target Gene Sets"
     status$finished <- ""
   })
-
+  
   # makes inputs for buttons on output results table
   shinyInput <- function(FUN, len, id, ...) {
     inputs <- character(len)
@@ -241,17 +347,19 @@ server <- function(input, output, session) {
     }
     inputs
   }
-
+  
   # produces the results dataframe with column of buttons
   tmptbl <- reactive({
     if (is.null(res())){
       return(NULL)
     }
-    data.frame(res()[[2]],
-                      Actions = shinyInput(actionButton, nrow(res()[[2]]), 'button_', label = "See Targets", onclick = 'Shiny.onInputChange(\"select_button\",  this.id)' )
-               )
+    tbl <- res()[[2]]
+    tbl[,2:3] <- round(as.numeric(tbl[,2:3]),4) 
+    data.frame(tbl,
+               Actions = shinyInput(actionButton, nrow(res()[[2]]), 'button_', label = "See Targets", onclick = 'Shiny.onInputChange(\"select_button\",  this.id)' )
+    )
   })
-
+  
   # renders the results dataframe with column of buttons
   output$resTable <- DT::renderDataTable({
     if (is.null(res())){
@@ -262,7 +370,7 @@ server <- function(input, output, session) {
   options = list(pageLength = 15, dom = 't'),
   colnames = c('NR','P Value','Adjusted P Value','Target Genes'))
   
-
+  
   # chooses the correct plot depending which button is pushed
   observeEvent(input$select_button, {
     if (is.null(res())){
@@ -272,7 +380,7 @@ server <- function(input, output, session) {
     selectedNR <- rownames(tmptbl())[selectedRow]
     chosenPlt(res()[[1]][[selectedNR]])
   })
-
+  
   # shows the selected table
   observe({
     if (is.null(res())){
@@ -286,9 +394,16 @@ server <- function(input, output, session) {
       chosenPlt()[[1]]
     },height = (3000/74)*chosenPlt()[[2]],width = 500)
     
+    # output$bplots <- renderPlot({
+    #   chosenPlt()[[1]]
+    # },width = "auto",
+    # height = function() {
+    #   session$clientData$output_bplots_width*(3000/74)*chosenPlt()[[2]]/500
+    # })
+    
   })
-
-
+  
+  
   # download buttons and handlers
   output$download1 <- renderUI({
     if(!is.null(res())) {
@@ -296,7 +411,7 @@ server <- function(input, output, session) {
                      style="color: #fff; background-color: #A9A9A9; border-color: #A9A9A9")
     }
   })
-
+  
   output$download2 <- renderUI({
     if(!is.null(res())) {
       downloadButton('downloadDiffex', label = "Differential Expression Results",
@@ -310,21 +425,21 @@ server <- function(input, output, session) {
                      style="color: #fff; background-color: #A9A9A9; border-color: #A9A9A9")
     }
   })
-
+  
   output$download4 <- renderUI({
     if(!is.null(chosenPlt())) {
       downloadButton('downloadPlot', label = "Boxplot Figure",
                      style="color: #fff; background-color: #A9A9A9; border-color: #A9A9A9")
     }
   })
-
+  
   output$downloadfRMA <- downloadHandler(
     filename = function() { paste('input_preprocessed', '.csv', sep='') },
     content = function(file) {
       write.csv(res()[[4]], file, row.names = FALSE)
     }
   )
-
+  
   output$downloadDiffex <- downloadHandler(
     filename = function() { paste('diff_exprs', '.csv', sep='') },
     content = function(file) {
@@ -338,7 +453,7 @@ server <- function(input, output, session) {
       write.csv(res()[[2]], file, row.names = FALSE)
     }
   )
-
+  
   output$downloadPlot <- downloadHandler(
     filename = function() { paste(chosenPlt()[[1]]$labels$title, '.png', sep='') },
     content = function(file) {
@@ -347,12 +462,9 @@ server <- function(input, output, session) {
     }
   )
   
-}
+  }
 
 shinyApp(ui, server)
-
-
-
 
 
 
